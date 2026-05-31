5d2a7b3ca820ce2706bbd38add50b76eae2b2890
amjundii <amjundii@gmail.com>
2026-05-31 09:40:06 +0800

    Firstcommit

---
+++ code/MenuAdd.php
@@ -0,0 +1,112 @@
+<!--
+Assignment 1, SCSM2223-25262 (MenuAdd.php)
+Group Name: ???
+-->
+<?php require 'libs/authpage.php'; ?>
+<?php require 'libs/db_connect_PDO.php'; ?>
+<?php
+
+$task = isset($_REQUEST['task']) ? $_REQUEST['task'] : "";
+$form_complete = FALSE;
+
+if ($task == "Cancel") {    
+} 
+else if ($task == "Add") {
+  $name = $_REQUEST['name'];
+  $type = $_REQUEST['type'];
+  $price = $_REQUEST['price'];
+  
+  $stmt = $pdo->prepare("INSERT INTO menus (name, type, price) VALUES (:name, :type, :price)");
+
+  try {
+    $stmt->execute([
+        ':name' => $name,
+        ':type' => $type,
+        ':price' => $price,
+    ]);
+    header("Location: MenuManage.php");
+    exit();
+  } catch (PDOException $ex) {
+    echo "Database Error: " . $ex->getMessage();
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tasty Bites - Add Menu</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="main_style.css"  rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<!-- Main Layout Table -->
+<table border="0" width="100%">
+  <!-- Header -->
+  <tr>
+    <td align="center">
+      <?php include 'libs/header.php'; ?>
+    </td>
+  </tr>
+
+  <!-- Navigation -->
+  <tr>
+    <td align="center">
+      <?php include 'libs/navigation.php'; ?>
+    </td>
+  </tr>
+
+  <!-- Content Row -->
+  <tr>
+    <!-- Menu Section -->
+    <td>
+      <h2>Add Menu Item</h2>
+      <table width="400" cellspacing="5">
+        <form action="MenuAdd.php" method="POST">
+          <tr>
+            <th align="right">Name:</th>
+            <td>
+              <input type="text" name="name" required>
+            </td>
+          </tr>
+          <tr>
+            <th align="right">Type:</th>
+            <td>
+              <select name="type">
+                <option value=""></option>
+                <option value="Drink">Drink</option>
+                <option value="Food">Food</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <th align="right">Price (RM):</th>
+            <td>
+              <input type="number" step="0.01" name="price" required>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              <input type="submit" name="task" value="Add"> 
+              <input type="button" onclick="history.back()" value="Cancel">
+            </td>
+          </tr>
+        </form>
+      </table>
+    </td>
+  </tr>
+
+  <!-- Footer -->
+  <tr>
+    <td align="center">
+      <?php include 'libs/footer.php'; ?>
+    </td>
+  </tr>
+
+</table>
+
+</body>
+</html>
---
+++ code/MenuUpdate.php
@@ -0,0 +1,142 @@
+<!--
+Lab2, SCSM2223-25262 (MenuUpdate.php)
+Group Name: ???
+-->
+<?php require 'libs/authpage.php'; ?>
+<?php require 'libs/db_connect_PDO.php'; ?>
+<?php
+
+if (empty($_REQUEST['id'])) { header("Location: MenuManage.php"); exit(); }
+$id = $_REQUEST['id'];
+$task = isset($_REQUEST['task']) ? $_REQUEST['task'] : "";
+
+if ($task == "Cancel") {
+} 
+else if ($task == "Update") {
+
+  $name = $_REQUEST['name'];
+  $type = $_REQUEST['type'];
+  $price = $_REQUEST['price'];
+  
+  $stmt = $pdo->prepare("UPDATE menus SET name = :name, type = :type, price = :price WHERE id = :id");
+
+  try {
+    $stmt->execute([
+        ':name' => $name,
+        ':type' => $type,
+        ':price' => $price,
+        ':id' => $id
+    ]);
+    header("Location: MenuManage.php");
+    exit();
+  } catch (PDOException $ex) {
+    echo "Database Error: " . $ex->getMessage();
+  }
+}
+
+// Select to display the menu data to be updated
+$stmt = $pdo->prepare("SELECT * FROM menus WHERE id = :id");
+
+try {
+  $stmt->execute([':id' => $id]);
+} catch (PDOException $ex) {
+  echo "Database Error: " . $ex->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tasty Bites - Update Menu</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="main_style.css"  rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<!-- Main Layout Table -->
+<table border="0" width="100%">
+  <!-- Header -->
+  <tr>
+    <td align="center">
+      <?php include 'libs/header.php'; ?>
+    </td>
+  </tr>
+
+  <!-- Navigation -->
+  <tr>
+    <td align="center">
+      <?php include 'libs/navigation.php'; ?>
+    </td>
+  </tr>
+
+  <!-- Content Row -->
+  <tr>
+    <!-- Menu Section -->
+    <td>
+      <h2>Update Menu Item</h2>
+      <table width="400" cellspacing="5" border="1">
+        <tr>
+          <th align="center">ID</th>
+          <th>Name</th>
+          <th align="center">Type</th>
+          <th align="right">Price (RM)</th>
+        </tr>
+        <?php
+        while ($row = $stmt->fetch()) { 
+        ?>
+          <tr>
+            <td align="center"><?= $row['id'] ?></td>
+            <td><?= $row['name'] ?></td>
+            <td align="center"><?= $row['type'] ?></td>
+            <td align="right"><?= number_format($row['price'], 2) ?></td>
+          </tr>
+        <?php
+        } 
+        ?>
+      </table>
+      <table width="400" cellspacing="5">
+        <form action="MenuUpdate.php" method="POST">
+          <input type="hidden" name="id" value="<?= $id ?>">
+          <tr>
+            <th align="right">Name:</th>
+            <td>
+              <input type="text" name="name" value="" required>
+            </td>
+          </tr>
+          <tr>
+            <th align="right">Type:</th>
+            <td>
+              <input type="text" name="type" value="" required>
+            </td>
+          </tr>
+          <tr>
+            <th align="right">Price (RM):</th>
+            <td>
+              <input type="number" step="0.01" name="price" value="" required>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>
+              <input type="submit" name="task" value="Update"> 
+              <input type="button" onclick="history.back()" value="Cancel">
+            </td>
+          </tr>
+        </form>
+      </table>
+    </td>
+  </tr>
+
+  <!-- Footer -->
+  <tr>
+    <td align="center">
+      <?php include 'libs/footer.php'; ?>
+    </td>
+  </tr>
+
+</table>
+
+</body>
+</html>
